6accd5ed5098585a35e172dcab58753935c60f54
ParsaSafavi <parsasafavi1384@gmail.com>
2026-05-13 01:35:06 +0330

    feat: define Store interface

---
+++ internal/store/store.go
@@ -0,0 +1,17 @@
+package store
+
+import (
+	"context"
+
+	"github.com/ParsaSafavi05/deploycrane/internal/model"
+)
+
+type Store interface{
+	Create(ctx context.Context, app model.App) error
+	Get(ctx context.Context, id string) (model.App, error)
+	Update(ctx context.Context, app model.App) error
+	List(ctx context.Context, ) ([]model.App, error)
+	Delete(ctx context.Context, id string) error 
+	Count(ctx context.Context, ) (int, error)
+}
+
