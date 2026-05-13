e51b7970d1d54202bbe107828ed844e49c3f1717
ParsaSafavi <parsasafavi1384@gmail.com>
2026-05-13 10:15:00 +0330

    feat: add Docker client helper

---
+++ internal/docker/client.go
@@ -0,0 +1,18 @@
+package docker
+
+import (
+	"context"
+
+	"github.com/moby/moby/client"
+)
+
+// NewClient creates a docker client using environment defaults and API version negotiation
+func NewClient() (*client.Client, error) {
+	return client.New(client.FromEnv)
+}
+
+// Checks whether the docker daemon is reachable
+func Ping(ctx context.Context, cli *client.Client) error  {
+	_, err := cli.Ping(ctx, client.PingOptions{})
+	return err
+}
