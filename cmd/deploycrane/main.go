69fa708db73e761ff7cb3e41acf60695bed157c1
ParsaSafavi <parsasafavi1384@gmail.com>
2026-05-14 14:11:36 +0330

    feat: api server implemented in main

--- cmd/deploycrane/main.go
+++ cmd/deploycrane/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/ParsaSafavi05/deploycrane/internal/api"
+	"github.com/ParsaSafavi05/deploycrane/internal/docker"
+	"github.com/ParsaSafavi05/deploycrane/internal/store"
 )
 
 func main() {
-	fmt.Println("DeployCrane - dockerizing and deploying your applications")	
+	// Create docker client
+	cli, err := docker.NewClient()
+	if err != nil {
+		log.Fatalf("failed to create docker client: %v", err)
+	}
+	
+	// Create in memory tore
+	storeInstance := store.NewInMemoryStore()
+
+	// Create server with all the dependencies
+	server := api.NewServer(cli, storeInstance)
+
+	// Get HTTP handler and start listening
+	handler := server.Handler()
+	
+	log.Println("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
