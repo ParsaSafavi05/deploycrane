0ff58f6464a677be77a8aaba94b01d85c861c5f2
ParsaSafavi05 <parsasafavi1384@gmail.com>
2026-05-16 16:14:39 +0330

    feat: container and host port to app struct

--- internal/models/app.go
+++ internal/models/app.go
@@ -7,22 +7,23 @@ type Status string
 const (
 	StatusCreated  Status = "created"
 	StatusCloning  Status = "cloning"
-	StatusCloned  Status = "cloned"
+	StatusCloned   Status = "cloned"
 	StatusBuilding Status = "building"
 	StatusRunning  Status = "running"
 	StatusStopped  Status = "stopped"
 	StatusFailed   Status = "failed"
-	StatusBuilt     Status = "built"
+	StatusBuilt    Status = "built"
 	StatusStarting Status = "starting"
 )
 
 type App struct {
-	ID          string    `json:"id"`
-	Name        string    `json:"name"`
-	RepoURL     string    `json:"repo_url"`
+	ID            string    `json:"id"`
+	Name          string    `json:"name"`
+	RepoURL       string    `json:"repo_url"`
 	ClonePath     string    `json:"clone_path,omitempty"`
-	Status      Status    `json:"status"`
-	ContainerID string    `json:"container_id,omitempty"`
-	Port        int       `json:"port,omitempty"`
-	CreatedAt   time.Time `json:"created_at"`
+	Status        Status    `json:"status"`
+	ContainerID   string    `json:"container_id,omitempty"`
+	ContainerPort int       `json:"container_port"`
+	HostPort      int       `json:"host_port"`
+	CreatedAt     time.Time `json:"created_at"`
 }
