9c0f9d33f78d7733960d682759448d43bed5d352
ParsaSafavi05 <parsasafavi1384@gmail.com>
2026-06-01 15:18:28 +0330

    change: set timeout duration to 30m

--- internal/config/config.go
+++ internal/config/config.go
@@ -15,7 +15,7 @@ type Config struct {
 	CloneBasePath     string        `envconfig:"DEPLOYCRANE_CLONE_BASE_PATH" default:"./clones"`
 	ShutdownTimeout   time.Duration `envconfig:"DEPLOYCRANE_SHUTDOWN_TIMEOUT" default:"30s"`
 	ReadTimeout       time.Duration `envconfig:"DEPLOYCRANE_HTTP_READ_TIMEOUT" default:"10s"`
-	WriteTimeout      time.Duration `envconfig:"DEPLOYCRANE_HTTP_WRITE_TIMEOUT" default:"15s"`
+	WriteTimeout      time.Duration `envconfig:"DEPLOYCRANE_HTTP_WRITE_TIMEOUT" default:"30m"`
 	IdleTimeout       time.Duration `envconfig:"DEPLOYCRANE_HTTP_IDLE_TIMEOUT" default:"60s"`
 	ImagePrefix       string        `envconfig:"DEPLOYCRANE_IMAGE_PREFIX" default:"deploycrane"`
 	ContainerPort     int           `envconfig:"DEPLOYCRANE_CONTAINER_PORT" default:"8080"`
